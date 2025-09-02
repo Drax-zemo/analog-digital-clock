c9be167689e9c5258567eb69d3c2ee36d94669c7
Akash <akashpandey9192@gmail.com>
2025-09-02 13:18:49 +0530

    script.js

--- script.js
+++ script.js
@@ -1,23 +1,27 @@
-let hour_hand= document.querySelector(".hour-hand")
-let min_hand= document.querySelector(".min-hand")
-let sec_hand= document.querySelector(".sec-hand")
-let dh= document.querySelector(".dh")
-let dm= document.querySelector(".dm")
-let ds= document.querySelector(".ds")
+let hour_hand = document.querySelector(".hour-hand");
+let min_hand = document.querySelector(".min-hand");
+let sec_hand = document.querySelector(".sec-hand");
+let dh = document.querySelector(".dh");
+let dm = document.querySelector(".dm");
+let ds = document.querySelector(".ds");
 
+setInterval(() => {
+  let time = new Date();
+  let h = time.getHours();
+  let m = time.getMinutes();
+  let s = time.getSeconds();
 
-setInterval(() =>{
-    let time =new Date();
-    let h = time.getHours();
-    let s = time.getMinutes();
-    let m = time.getSeconds();
-    let hrotation=30*h+h/2;
-    let mrotation=6*m;
-    let srotation=6*s;
-    hour_hand.style.transform=`rotate(${hrotation}deg)`
-    min_hand.style.transform=`rotate(${mrotation}deg)`
-    sec_hand.style.transform=`rotate(${srotation}deg)`
-   dh.innerHTML=(h<10?'0'+h:h)>12?hh-=12:h 
-   dm.innerHTML=m<10?'0'+m:m
-   ds.innerHTML=s<10?'0'+s:s
-})
+  // Analog
+  let hrotation = 30 * h + m / 2;
+  let mrotation = 6 * m;
+  let srotation = 6 * s;
+  hour_hand.style.transform = `rotate(${hrotation}deg)`;
+  min_hand.style.transform = `rotate(${mrotation}deg)`;
+  sec_hand.style.transform = `rotate(${srotation}deg)`;
+
+  // Digital
+  let displayH = h % 12 || 12;
+  dh.innerHTML = displayH < 10 ? "0" + displayH : displayH;
+  dm.innerHTML = m < 10 ? "0" + m : m;
+  ds.innerHTML = s < 10 ? "0" + s : s;
+}, 1000);
